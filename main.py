5fa1961755e8fce64cd19e89a270891777e65707
EmilyCosta <56204776+EmilyCosta@users.noreply.github.com>
2024-08-22 22:43:12 +0000

    Criação imagem docker e mudança em main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("inicio")
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Carregar o modelo treinado (assumindo que você o salvou como 'model.joblib')
+model = joblib.load("model.joblib")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    prediction = model.predict(np.array(data['input']).reshape(1, -1))
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
